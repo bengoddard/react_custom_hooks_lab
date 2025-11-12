0583300c79fe5382684e16b8f37b6a1f2df9dfa9
Ben Goddard <goddard.ben04@gmail.com>
2025-11-12 15:15:58 -0700

    Added cutsom hook and connected it to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,18 @@
 import { useLocalStorage } from "../hooks/useLocalStorage";
 function Form() {
-
+  const [name, setName] = useLocalStorage("name",  );
+  const [serviceNumber, setServiceNumber] = useLocalStorage("serviceNumber", )
     return (
       <>
         <form style={{ display: "flex", flexDirection: "column" }}>
             <label htmlFor="name">Name:</label>
-            <input type="text" data-testid={"name"} />
+            <input type="text" data-testid={"name"} onChange={(e) => setName(e.target.value)}/>
             <label htmlFor="service">Service Number:</label>
-            <input type="text" data-testid={"service"} />
+            <input type="text" data-testid={"service"} onChange={(e) => setServiceNumber(e.target.value)}/>
 
         </form>
         <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
+        <h4>{serviceNumber ? `Here's your service number ${serviceNumber}` : "Enter your service number" }</h4>
       </>
     );
 }
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const [value, setValue] = useState(() => {
+        const stored = localStorage.getItem(key);
+        return stored !== null ? stored : initialValue;
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [key, value]);
+
+    return [value, setValue];
 }
